1fef67585edbf63832791a0d6db3ccfa9248c204
Michael Lamparski <diagonaldevice@gmail.com>
2019-06-14 14:15:16 -0400

    bench NpyWriter instead of TypeWrite

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,49 +3,32 @@
 extern crate npy;
 extern crate test;
 
-use npy::{Serialize, Deserialize, AutoSerialize, TypeWrite, TypeRead};
+use npy::{Serialize, Deserialize, AutoSerialize, NpyWriter, OutFile};
 use test::Bencher;
 use test::black_box as bb;
+use std::io::Cursor;
 
 const NITER: usize = 100_000;
 
 macro_rules! gen_benches {
     ($T:ty, $new:expr) => {
         #[inline(never)]
-        fn test_data() -> Vec<u8> {
-            let mut raw = Vec::new();
-            let writer = <$T>::writer(&<$T>::default_dtype()).unwrap();
-            for i in 0usize..NITER {
-                writer.write_one(&mut raw, &$new(i)).unwrap();
-            }
-            raw
-        }
-
-        #[bench]
-        fn read(b: &mut Bencher) {
-            let raw = test_data();
-            b.iter(|| {
-                let dtype = <$T>::default_dtype();
-                let reader = <$T>::reader(&dtype).unwrap();
-
-                let mut remainder = &raw[..];
-                for _ in 0usize..NITER {
-                    let (value, new_remainder) = reader.read_one(remainder);
-                    bb(value);
-                    remainder = new_remainder;
+        fn write_array_via_push() -> Vec<u8> {
+            let cap = 1000 + <$T>::default_dtype().num_bytes() * NITER;
+            let mut cursor = Cursor::new(Vec::with_capacity(cap));
+            {
+                let mut writer = NpyWriter::begin(&mut cursor).unwrap();
+                for i in 0usize..NITER {
+                    writer.push(&$new(i)).unwrap();
                 }
-                assert_eq!(remainder.len(), 0);
-            });
+                writer.finish().unwrap();
+            }
+            cursor.into_inner()
         }
 
         #[bench]
         fn read_to_vec(b: &mut Bencher) {
-            // FIXME: Write to a Cursor<Vec<u8>> once #16 is merged
-            let path = concat!("benches/bench_", stringify!($T), ".npy");
-
-            npy::to_file(path, (0usize..NITER).map($new)).unwrap();
-            let bytes = std::fs::read(path).unwrap();
-
+            let bytes = write_array_via_push();
             b.iter(|| {
                 bb(npy::NpyData::<$T>::from_bytes(&bytes).unwrap().to_vec())
             });
@@ -54,7 +37,7 @@ macro_rules! gen_benches {
         #[bench]
         fn write(b: &mut Bencher) {
             b.iter(|| {
-                bb(test_data())
+                bb(write_array_via_push())
             });
         }
     };
