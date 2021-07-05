65822eff47841924cc12ea4114c580a83b9584c4
Michael Lamparski <diagonaldevice@gmail.com>
2021-07-05 15:37:19 -0400

    finish tests for sparse reading

--- tests/sparse.rs
+++ tests/sparse.rs
@@ -2,7 +2,7 @@ use npyz::npz::NpzArchive;
 use npyz::sparse;
 
 fn open_test_npz(name: &str) -> NpzArchive<std::io::BufReader<std::fs::File>> {
-    NpzArchive::open(format!("test-data/{}", name)).unwrap()
+    NpzArchive::open(format!("test-data/sparse/{}", name)).unwrap()
 }
 
 // Python code to create NPZs:
@@ -10,15 +10,15 @@ fn open_test_npz(name: &str) -> NpzArchive<std::io::BufReader<std::fs::File>> {
 //   import numpy as np
 //   import scipy.sparse as ss
 //   m = np.array([[1, 0, 4, 0, 0, 0], [0, 2, 0, 0, 0, 0], [6, 0, 7, 0, 0, 0]])
-//   ss.save_npz('test-data/sparse-coo.npz', ss.coo_matrix(m))
-//   ss.save_npz('test-data/sparse-csr.npz', ss.csr_matrix(m))
-//   ss.save_npz('test-data/sparse-csc.npz', ss.csc_matrix(m))
-//   ss.save_npz('test-data/sparse-dia.npz', ss.dia_matrix(m))
-//   ss.save_npz('test-data/sparse-bsr.npz', ss.bsr_matrix(m, blocksize=(1,2)))
+//   ss.save_npz('test-data/sparse/coo.npz', ss.coo_matrix(m))
+//   ss.save_npz('test-data/sparse/csr.npz', ss.csr_matrix(m))
+//   ss.save_npz('test-data/sparse/csc.npz', ss.csc_matrix(m))
+//   ss.save_npz('test-data/sparse/dia.npz', ss.dia_matrix(m))
+//   ss.save_npz('test-data/sparse/bsr.npz', ss.bsr_matrix(m, blocksize=(1,2)))
 
 #[test]
 fn read_sparse_coo() {
-    let m = sparse::Coo::<i64>::from_npz(&mut open_test_npz("sparse-coo.npz")).unwrap();
+    let m = sparse::Coo::<i64>::from_npz(&mut open_test_npz("coo.npz")).unwrap();
     assert_eq!(m, sparse::Coo {
         shape: [3, 6],
         data: vec![1, 4, 2, 6, 7],
@@ -29,7 +29,7 @@ fn read_sparse_coo() {
 
 #[test]
 fn read_sparse_csr() {
-    let m = sparse::Csr::<i64>::from_npz(&mut open_test_npz("sparse-csr.npz")).unwrap();
+    let m = sparse::Csr::<i64>::from_npz(&mut open_test_npz("csr.npz")).unwrap();
     assert_eq!(m, sparse::Csr {
         shape: [3, 6],
         data: vec![1, 4, 2, 6, 7],
@@ -40,7 +40,7 @@ fn read_sparse_csr() {
 
 #[test]
 fn read_sparse_csc() {
-    let m = sparse::Csc::<i64>::from_npz(&mut open_test_npz("sparse-csc.npz")).unwrap();
+    let m = sparse::Csc::<i64>::from_npz(&mut open_test_npz("csc.npz")).unwrap();
     assert_eq!(m, sparse::Csc {
         shape: [3, 6],
         data: vec![1, 6, 2, 4, 7],
@@ -51,7 +51,7 @@ fn read_sparse_csc() {
 
 #[test]
 fn read_sparse_dia() {
-    let m = sparse::Dia::<i64>::from_npz(&mut open_test_npz("sparse-dia.npz")).unwrap();
+    let m = sparse::Dia::<i64>::from_npz(&mut open_test_npz("dia.npz")).unwrap();
     assert_eq!(m, sparse::Dia {
         shape: [3, 6],
         offsets: vec![-2, 0, 2],
@@ -65,7 +65,7 @@ fn read_sparse_dia() {
 
 #[test]
 fn read_sparse_bsr() {
-    let m = sparse::Bsr::<i64>::from_npz(&mut open_test_npz("sparse-bsr.npz")).unwrap();
+    let m = sparse::Bsr::<i64>::from_npz(&mut open_test_npz("bsr.npz")).unwrap();
     assert_eq!(m, sparse::Bsr {
         shape: [3, 6],
         blocksize: [1, 2],
@@ -83,50 +83,90 @@ fn read_sparse_bsr() {
 fn read_sparse_dynamic() {
     use sparse::Sparse;
 
-    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("sparse-coo.npz")).unwrap(), Sparse::Coo(_)));
-    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("sparse-csr.npz")).unwrap(), Sparse::Csr(_)));
-    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("sparse-csc.npz")).unwrap(), Sparse::Csc(_)));
-    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("sparse-bsr.npz")).unwrap(), Sparse::Bsr(_)));
-    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("sparse-dia.npz")).unwrap(), Sparse::Dia(_)));
+    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("coo.npz")).unwrap(), Sparse::Coo(_)));
+    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("csr.npz")).unwrap(), Sparse::Csr(_)));
+    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("csc.npz")).unwrap(), Sparse::Csc(_)));
+    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("bsr.npz")).unwrap(), Sparse::Bsr(_)));
+    assert!(matches!(Sparse::<i64>::from_npz(&mut open_test_npz("dia.npz")).unwrap(), Sparse::Dia(_)));
 }
 
 #[test]
 fn read_wrong_format_err() {
-    sparse::Csr::<i64>::from_npz(&mut open_test_npz("sparse-coo.npz")).unwrap_err();
+    let err = sparse::Csr::<i64>::from_npz(&mut open_test_npz("coo.npz")).unwrap_err();
+    assert!(err.to_string().contains("format"));
 }
 
 #[test]
 fn read_sparse_with_long_indices() {
-    // - test successful read of i64 indices in CSR
-    unimplemented!()
+    let m = sparse::Coo::<i64>::from_npz(&mut open_test_npz("coo-long.npz")).unwrap();
+    assert_eq!(m, sparse::Coo {
+        shape: [0x8_0000_0000, 0x10_0000_0000],
+        data: vec![3, 1],
+        row: vec![0x4_0000_0000, 3],
+        col: vec![4, 0x2_0000_0000],
+    });
 }
 
 #[test]
 fn read_sparse_dia_with_long_offsets() {
-    // - test successful read of i64 offsets in DIA
-    unimplemented!()
+    let m = sparse::Dia::<i64>::from_npz(&mut open_test_npz("dia-long.npz")).unwrap();
+    assert_eq!(m, sparse::Dia {
+        shape: [0x8_0000_0000, 2],
+        offsets: vec![-0x8_0000_0000 + 3],
+        data: vec![0, 1],
+    });
 }
 
 #[test]
 fn read_sparse_coo_with_dupes() {
-    // - test successful read of COO with duplicates
-    unimplemented!()
+    let m = sparse::Coo::<i64>::from_npz(&mut open_test_npz("coo-dupes.npz")).unwrap();
+    assert_eq!(m, sparse::Coo {
+        shape: [5, 5],
+        data: vec![10, 20],
+        row: vec![2, 2],
+        col: vec![3, 3],
+    });
 }
 
 #[test]
 fn read_sparse_csr_unsorted() {
-    // - test successful read of CSR with unsorted columns
-    unimplemented!()
+    // python:
+    //   import scipy.sparse as ss
+    //   m=ss.csr_matrix([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
+    //   ss.save_npz('test-data/sparse/csr-unsorted.npz', m @ m)
+    let m = sparse::Csr::<i64>::from_npz(&mut open_test_npz("csr-unsorted.npz")).unwrap();
+    assert_eq!(m, sparse::Csr {
+        shape: [3, 3],
+        data: vec![2, 2, 1, 2, 2],
+        indices: vec![2, 0, 1, 2, 0],
+        indptr: vec![0, 2, 3, 5],
+    });
 }
 
 #[test]
 fn read_fortran_order_err() {
-    // - fortran order error
-    unimplemented!()
+    // python:
+    //   import numpy as np
+    //   npz = np.load('test-data/sparse/bsr.npz')
+    //   mats = dict(**npz)
+    //
+    //   assert not mats['data'].flags['F_CONTIGUOUS']
+    //   mats['data'] = mats['data'].T.copy().T
+    //   assert mats['data'].flags['F_CONTIGUOUS']
+    //
+    //   np.savez('test-data/sparse/bsr-f-order.npz', **mats)
+    let err = sparse::Bsr::<i64>::from_npz(&mut open_test_npz("bsr-f-order.npz")).unwrap_err();
+    assert!(err.to_string().contains("ortran"));
 }
 
 #[test]
 fn read_bad_dimension_err() {
-    // - error when data of bsr is not 3d
-    unimplemented!()
+    // python:
+    //   import numpy as np
+    //   npz = np.load('test-data/sparse/bsr.npz')
+    //   mats = dict(**npz)
+    //   mats['data'] = mats['data'].reshape((-1, 2))
+    //   np.savez('test-data/sparse/bsr-bad-ndim.npz', **mats)
+    let err = sparse::Bsr::<i64>::from_npz(&mut open_test_npz("bsr-bad-ndim.npz")).unwrap_err();
+    assert!(err.to_string().contains("ndim"));
 }
